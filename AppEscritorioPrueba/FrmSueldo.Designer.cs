37c8de14e7b6fe88d272184fe2eed80891fa5aec
BRYAN GASPAR <bryan.gaspar@pucese.edu.ec>
2020-11-16 10:25:20 -0500

    Diseñando Interfaz Sueldo de Trabajador

--- AppEscritorioPrueba/FrmSueldo.Designer.cs
+++ AppEscritorioPrueba/FrmSueldo.Designer.cs
@@ -29,19 +29,293 @@ namespace AppEscritorioPrueba
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmSueldo));
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtHorasTrab = new System.Windows.Forms.TextBox();
+            this.txtValorHora = new System.Windows.Forms.TextBox();
+            this.txtBono = new System.Windows.Forms.TextBox();
+            this.txtTotalIng = new System.Windows.Forms.TextBox();
+            this.txtTotalEgre = new System.Windows.Forms.TextBox();
+            this.txtCuentaPag = new System.Windows.Forms.TextBox();
+            this.txtBar = new System.Windows.Forms.TextBox();
+            this.txtAsoTrab = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Verdana", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(250, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(427, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Calculo del sueldo de un trabajador";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.txtTotalIng);
+            this.groupBox1.Controls.Add(this.txtBono);
+            this.groupBox1.Controls.Add(this.txtValorHora);
+            this.groupBox1.Controls.Add(this.txtHorasTrab);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.groupBox1.Location = new System.Drawing.Point(39, 154);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(331, 239);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ingresos";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox2.Controls.Add(this.txtTotalEgre);
+            this.groupBox2.Controls.Add(this.txtAsoTrab);
+            this.groupBox2.Controls.Add(this.txtCuentaPag);
+            this.groupBox2.Controls.Add(this.label10);
+            this.groupBox2.Controls.Add(this.txtBar);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.groupBox2.Location = new System.Drawing.Point(386, 154);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(334, 239);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Egresos";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Verdana", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(74, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(214, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nombre del Trabajador";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(307, 108);
+            this.txtNombre.MaxLength = 50;
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(257, 20);
+            this.txtNombre.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Verdana", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(18, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(127, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Horas Trabajadas:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Verdana", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(18, 89);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Valor por Hora:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Verdana", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(14, 139);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(171, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Bono de responsabilidad:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Verdana", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(19, 186);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(126, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Total de Ingresos:";
+            // 
+            // txtHorasTrab
+            // 
+            this.txtHorasTrab.Location = new System.Drawing.Point(191, 42);
+            this.txtHorasTrab.Name = "txtHorasTrab";
+            this.txtHorasTrab.Size = new System.Drawing.Size(111, 26);
+            this.txtHorasTrab.TabIndex = 9;
+            // 
+            // txtValorHora
+            // 
+            this.txtValorHora.Location = new System.Drawing.Point(191, 86);
+            this.txtValorHora.Name = "txtValorHora";
+            this.txtValorHora.Size = new System.Drawing.Size(111, 26);
+            this.txtValorHora.TabIndex = 10;
+            // 
+            // txtBono
+            // 
+            this.txtBono.Location = new System.Drawing.Point(191, 131);
+            this.txtBono.Name = "txtBono";
+            this.txtBono.Size = new System.Drawing.Size(111, 26);
+            this.txtBono.TabIndex = 11;
+            // 
+            // txtTotalIng
+            // 
+            this.txtTotalIng.Location = new System.Drawing.Point(191, 178);
+            this.txtTotalIng.Name = "txtTotalIng";
+            this.txtTotalIng.ReadOnly = true;
+            this.txtTotalIng.Size = new System.Drawing.Size(111, 26);
+            this.txtTotalIng.TabIndex = 12;
+            // 
+            // txtTotalEgre
+            // 
+            this.txtTotalEgre.Location = new System.Drawing.Point(179, 178);
+            this.txtTotalEgre.Name = "txtTotalEgre";
+            this.txtTotalEgre.ReadOnly = true;
+            this.txtTotalEgre.Size = new System.Drawing.Size(111, 26);
+            this.txtTotalEgre.TabIndex = 20;
+            // 
+            // txtCuentaPag
+            // 
+            this.txtCuentaPag.Location = new System.Drawing.Point(179, 131);
+            this.txtCuentaPag.Name = "txtCuentaPag";
+            this.txtCuentaPag.Size = new System.Drawing.Size(111, 26);
+            this.txtCuentaPag.TabIndex = 19;
+            // 
+            // txtBar
+            // 
+            this.txtBar.Location = new System.Drawing.Point(179, 86);
+            this.txtBar.Name = "txtBar";
+            this.txtBar.Size = new System.Drawing.Size(111, 26);
+            this.txtBar.TabIndex = 18;
+            // 
+            // txtAsoTrab
+            // 
+            this.txtAsoTrab.Location = new System.Drawing.Point(179, 42);
+            this.txtAsoTrab.Name = "txtAsoTrab";
+            this.txtAsoTrab.Size = new System.Drawing.Size(111, 26);
+            this.txtAsoTrab.TabIndex = 17;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Verdana", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(7, 186);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(119, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Total de Egresos:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("Verdana", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(7, 139);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(130, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Cuentas por pagar:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.Transparent;
+            this.label9.Font = new System.Drawing.Font("Verdana", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(7, 86);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(33, 13);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "Bar:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.Color.Transparent;
+            this.label10.Font = new System.Drawing.Font("Verdana", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(7, 50);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(150, 13);
+            this.label10.TabIndex = 13;
+            this.label10.Text = "Aso. de Trabajadores:";
             // 
             // FrmSueldo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
             this.Name = "FrmSueldo";
-            this.Text = "CalculoDelsueldoProgramados";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CalculoDelsueldoTrabajador";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtTotalIng;
+        private System.Windows.Forms.TextBox txtBono;
+        private System.Windows.Forms.TextBox txtValorHora;
+        private System.Windows.Forms.TextBox txtHorasTrab;
+        private System.Windows.Forms.TextBox txtTotalEgre;
+        private System.Windows.Forms.TextBox txtAsoTrab;
+        private System.Windows.Forms.TextBox txtCuentaPag;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtBar;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
     }
 }
