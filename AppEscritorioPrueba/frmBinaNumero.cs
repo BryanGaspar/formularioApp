d63b5d248d11a90719e1cbd3ed3b2a63d89075f9
BRYAN GASPAR <bryan.gaspar@pucese.edu.ec>
2020-11-19 12:55:33 -0500

    Binario a Decimal 2

--- AppEscritorioPrueba/frmBinaNumero.cs
+++ AppEscritorioPrueba/frmBinaNumero.cs
@@ -19,47 +19,47 @@ namespace AppEscritorioPrueba
 
         private void btnCalcular_Click(object sender, EventArgs e)
         {
-            double residuo = 0;
+            double res = 0;
             double suma = 0;
             if (checkBox8.Checked == true)
             {
-                residuo = Math.Pow(2, 0);
-                suma += residuo;
+                res = Math.Pow(2, 0);
+                suma += res;
             }
             if (checkBox7.Checked == true)
             {
-                residuo = Math.Pow(2, 1);
-                suma += residuo;
+                res = Math.Pow(2, 1);
+                suma += res;
             }
             if (checkBox6.Checked == true)
             {
-                residuo = Math.Pow(2, 2);
-                suma += residuo;
+                res = Math.Pow(2, 2);
+                suma += res;
             }
             if (checkBox5.Checked == true)
             {
-                residuo = Math.Pow(2, 3);
-                suma += residuo;
+                res = Math.Pow(2, 3);
+                suma += res;
             }
             if (checkBox4.Checked == true)
             {
-                residuo = Math.Pow(2, 4);
-                suma += residuo;
+                res = Math.Pow(2, 4);
+                suma += res;
             }
             if (checkBox3.Checked == true)
             {
-                residuo = Math.Pow(2, 5);
-                suma += residuo;
+                res = Math.Pow(2, 5);
+                suma += res;
             }
             if (checkBox2.Checked == true)
             {
-                residuo = Math.Pow(2, 6);
-                suma += residuo;
+                res = Math.Pow(2, 6);
+                suma += res;
             }
             if (checkBox1.Checked == true)
             {
-                residuo = Math.Pow(2, 7);
-                suma += residuo;
+                res = Math.Pow(2, 7);
+                suma += res;
             }
             txtNum.Text = suma.ToString();
         }
